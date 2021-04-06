b250a9f543bd7b99a41409ad80d5b8713b1ec274
Lord_Alfred <lord-alfred@yandex.ru>
2021-04-06 23:30:44 +0300

    Удалил дублирование языков

--- src/dnlp/postprocess.py
+++ src/dnlp/postprocess.py
@@ -1,4 +1,4 @@
-from dnlp.languages import FASTTEXT_LANGUAGES, FASTTEXT_LANGUAGES_EXTRA_DATA
+from dnlp.languages import FASTTEXT_LANGUAGES
 
 
 def remap_prediction(prediction):
@@ -12,13 +12,13 @@ def remap_prediction(prediction):
         result.append({
             'confidence': prediction[1][index],
             'code': lang,
-            'name': FASTTEXT_LANGUAGES[lang],
-            'family': FASTTEXT_LANGUAGES_EXTRA_DATA[lang][0],
-            'endonym': FASTTEXT_LANGUAGES_EXTRA_DATA[lang][2],
-            'iso639-1': FASTTEXT_LANGUAGES_EXTRA_DATA[lang][3],
-            'iso639-2/T': FASTTEXT_LANGUAGES_EXTRA_DATA[lang][4],
-            'iso639-2/B': FASTTEXT_LANGUAGES_EXTRA_DATA[lang][5],
-            'iso639-3': FASTTEXT_LANGUAGES_EXTRA_DATA[lang][6],
+            'name': FASTTEXT_LANGUAGES[lang][1],
+            'family': FASTTEXT_LANGUAGES[lang][0],
+            'endonym': FASTTEXT_LANGUAGES[lang][2],
+            'iso639-1': FASTTEXT_LANGUAGES[lang][3],
+            'iso639-2/T': FASTTEXT_LANGUAGES[lang][4],
+            'iso639-2/B': FASTTEXT_LANGUAGES[lang][5],
+            'iso639-3': FASTTEXT_LANGUAGES[lang][6],
         })
 
     return sorted(result, key=lambda d: d['confidence'], reverse=True)
